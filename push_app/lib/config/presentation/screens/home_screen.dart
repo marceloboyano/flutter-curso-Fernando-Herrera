8ec9d151e8eb1bf6ba4fe4f8999a7a2ec1b9837f
Marcelo Boyanosky <104164605+marceloboyano@users.noreply.github.com>
2023-06-19 12:57:39 -0300

    up

--- push_app/lib/config/presentation/screens/home_screen.dart
+++ push_app/lib/config/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/config/presentation/screens/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
    
@@ -8,7 +10,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('Permisos'),
+        title:context.select((NotificationsBloc bloc) =>  Text('${bloc.state.status}')),
+         
         actions: [
           IconButton(onPressed: () {
             
